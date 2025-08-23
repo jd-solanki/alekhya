db9db10a0422484ad715f5b1494060d784d27dc9
JD Solanki <jdsolanki0001@gmail.com>
2025-08-23 19:35:21 +0530

    feat: updates as per shiki-image new version

--- server/api/image/code.get.ts
+++ server/api/image/code.get.ts
@@ -1,21 +1,65 @@
+import type { BundledLanguage, BundledTheme } from 'shiki'
 import { Buffer } from 'node:buffer'
-import { codeToImage } from 'shiki-image'
+import { Renderer } from '@takumi-rs/core'
+import { bundledLanguages, bundledThemes, createHighlighter } from 'shiki'
+import { codeToImageCore } from 'shiki-image/core'
+import { createOnigurumaEngine } from 'shiki/engine/oniguruma'
+
+const shikiThemes = Object.keys(bundledThemes)
+const shikiLangs = Object.keys(bundledLanguages)
+
+const DEFAULT_FONT
+  = 'https://fonts.bunny.net/jetbrains-mono/files/jetbrains-mono-latin-400-normal.woff2'
+
+export async function loadFont(font: string | ArrayBuffer | undefined) {
+  let fontData: ArrayBuffer
+  if (!font) {
+    font = DEFAULT_FONT
+  }
+  if (typeof font === 'string') {
+    const fontCache: Map<string, ArrayBuffer> = ((
+      globalThis as any
+    ).__shikiImageFontCache__ ||= new Map())
+    fontData
+      = fontCache.get(font) || (await fetch(font).then(r => r.arrayBuffer()))
+    fontCache.set(font, fontData)
+  }
+  else {
+    fontData = font
+  }
+  return fontData
+}
 
 export default defineEventHandler(async (event) => {
   // Get code from query parameters, with fallback to default
   const query = getQuery(event)
-  const theme = query.theme || 'dracula'
-  const lang = query.lang || 'js'
+
+  const queryTheme = typeof query.theme === 'string' ? query.theme : 'dracula'
+  const theme = shikiThemes.includes(queryTheme) ? queryTheme as BundledTheme : 'dracula'
+
+  // const queryLang = query.lang || 'js'
+  const lang = typeof query.lang === 'string' ? query.lang : 'js'
+  if (!shikiLangs.includes(lang)) {
+    throw createError({ statusCode: 400, statusMessage: 'Invalid language' })
+  }
+
   const encodedCode = (query.code as string) || 'Y29uc29sZS5sb2coJ0hlbGxvLCB3b3JsZCEnKTs='
   const code = Buffer.from(encodedCode, 'base64').toString('utf-8')
 
-  // Generate the code image
-  const buffer = await codeToImage(code, {
-    lang: lang as any,
-    theme: theme as any,
-    style: { borderRadius: 0 },
-    font: 'https://fonts.bunny.net/ubuntu-sans-mono/files/ubuntu-sans-mono-latin-400-normal.woff2',
+  const highlighter = await createHighlighter({
+    themes: [theme],
+    langs: [lang],
+    engine: createOnigurumaEngine(() => import('shiki/wasm')),
   })
+
+  const fontData = await loadFont(DEFAULT_FONT)
+  const renderer = new Renderer({ fonts: [fontData] })
+
+  // Generate the code image
+  const buffer = await codeToImageCore(code, {
+    lang: lang as BundledLanguage,
+    theme,
+  }, { highlighter, renderer })
 
   // Set the response headers for PNG image
   setHeader(event, 'Content-Type', 'image/png')
